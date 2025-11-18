a53f57bd481f1a19f9fe1f0b2c38d64c4d262ef8
itsmynt <markx.mt@googlemail.com>
2025-11-19 00:15:28 +0100

    Update Plugin.cs

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,8 +1,7 @@
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
-// Die Zeile "using Microsoft.Extensions.DependencyInjection;" brauchen wir hier nicht mehr
-// Die Zeile "using Jellyfin.ABRHls.Services;" brauchen wir hier auch nicht mehr
+using MediaBrowser.Model.Serialization; // <--- Das hier hat gefehlt!
 
 namespace Jellyfin.ABRHls;
 
@@ -11,22 +10,21 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     public override string Name => "ABR HLS Cinema";
     public override Guid Id { get; } = Guid.Parse("b91b3f1d-6c74-4f2e-9e9d-6a27b9a2f3d1");
 
+    // IXmlSerializer wird hier benutzt, braucht oben das using
     public Plugin(IApplicationPaths appPaths, IXmlSerializer xmlSerializer) : base(appPaths, xmlSerializer) { }
-
-    // Hier stand früher RegisterServices - jetzt ist es weg, weil PluginServiceRegistrator das macht.
 
     public IEnumerable<PluginPageInfo> GetPages()
     {
-        yield return new PluginPageInfo
-        {
-            Name = "abr-player.html",
-            EmbeddedResourcePath = GetType().Namespace + ".web.abr-player.html"
+        yield return new PluginPageInfo 
+        { 
+            Name = "abr-player.html", 
+            EmbeddedResourcePath = GetType().Namespace + ".web.abr-player.html" 
         };
-
-        yield return new PluginPageInfo
-        {
-            Name = "abr-player.js",
-            EmbeddedResourcePath = GetType().Namespace + ".web.abr-player.js"
+        
+        yield return new PluginPageInfo 
+        { 
+            Name = "abr-player.js", 
+            EmbeddedResourcePath = GetType().Namespace + ".web.abr-player.js" 
         };
     }
 }
