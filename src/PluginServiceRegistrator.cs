d638cc80eb671aa9bb744605aa49be4e302dcf2d
itsmynt <markx.mt@googlemail.com>
2025-11-19 21:51:42 +0100

    Refactor PluginServiceRegistrator for cleaner code

--- src/PluginServiceRegistrator.cs
+++ src/PluginServiceRegistrator.cs
@@ -9,10 +9,7 @@ public class PluginServiceRegistrator : IPluginServiceRegistrator
 {
     public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
     {
-        // Packager als Singleton (wird überall verwendet)
         serviceCollection.AddSingleton<HlsPackager>();
-        
-        // Watcher als Hosted Service (läuft im Hintergrund mit)
         serviceCollection.AddHostedService<LibraryWatcher>();
     }
 }
