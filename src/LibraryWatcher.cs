cacd31703448cc71971157913195a7081e3b0963
itsmynt <markx.mt@googlemail.com>
2025-11-19 22:40:49 +0100

    Update LibraryWatcher.cs

--- src/LibraryWatcher.cs
+++ src/LibraryWatcher.cs
@@ -14,7 +14,9 @@ public class LibraryWatcher : BackgroundService
 
     public LibraryWatcher(ILogger<LibraryWatcher> log, ILibraryManager lib, HlsPackager pack)
     {
-        _log = log; _lib = lib; _pack = pack; _plugin = Plugin.Instance!;
+        _log = log; _lib = lib; _pack = pack; 
+        _plugin = Plugin.Instance!; 
+        
         _lib.ItemAdded += OnItemAdded;
     }
 
